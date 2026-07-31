27b2d2e81fcd4b773cd95e6fda1f80b6c3a247be
NicholasPimenta <nicholas2ilva@gmail.com>
2026-07-30 23:13:39 -0300

    Deletion of files and addition of new ones

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,5 @@
-import { Navigate, Route, Routes } from "react-router";
-
-import { DashboardLayout } from "@/layouts/DashboardLayout";
-import { Agenda } from "@/pages/Agenda";
-import { Dashboard } from "@/pages/Dashboard";
-import { Patients } from "@/pages/Patients";
-
 function App() {
-  return (
-    <Routes>
-      <Route element={<DashboardLayout />}>
-        <Route index element={<Dashboard />} />
-        <Route path="agenda" element={<Agenda />} />
-        <Route path="pacientes" element={<Patients />} />
-      </Route>
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+  return <h1>Hello World</h1>
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,11 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
 
-import App from "./App";
-import "./index.css";
+import App from "./App"
+import "./index.css"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-);
+    <App />
+  </StrictMode>
+)
