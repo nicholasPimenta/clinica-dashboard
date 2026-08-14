39a07a8fd6f4fafae5d452163aca380b6f4455e6
NicholasPimenta <nicholas2ilva@gmail.com>
2026-08-13 21:17:19 -0300

    Implement Checkbox Exclusivity Rules

--- src/pages/DocumentGenerator.tsx
+++ src/pages/DocumentGenerator.tsx
@@ -16,14 +16,41 @@ import {
   FieldSet,
 } from "@/components/ui/field"
 import { Textarea } from "@/components/ui/textarea"
+import { useState } from "react"
 
 const exames = ["Lorem", "Ipsum", "Dolor", "Sit", "Amet"] as const
 
 function DocumentGenerator() {
+  const [habitos, setHabitos] = useState({
+    habitosSaudaveis: false,
+    tabagismo: false,
+    sedentarismo: false,
+    etilismo: false,
+  })
+
+  const [historicoCronico, setHistoricoCronico] = useState({
+    dm: false,
+    has: false,
+    dislipidemia: false,
+    nenhuma: false,
+  })
+
+  const [historicoFamiliar, setHistoricoFamiliar] = useState({
+    dm: false,
+    has: false,
+    dislipidemia: false,
+    coronariana: false,
+    nenhuma: false,
+  })
+
   return (
     <main className="container mx-auto py-8">
       <header>
-        <img src="img/LogoProvisorio.png" alt="Logo" className="mx-auto my-4 size-1/4" />
+        <img
+          src="img/LogoProvisorio.png"
+          alt="Logo"
+          className="mx-auto my-4 size-1/4"
+        />
       </header>
       <section>
         <div>
@@ -58,6 +85,13 @@ function DocumentGenerator() {
                   id="habitos-saudaveis"
                   name="habitos-saudaveis"
                   className="cursor-pointer"
+                  checked={habitos.habitosSaudaveis}
+                  onCheckedChange={(checked) =>
+                    setHabitos((habitosAtuais) => ({
+                      ...habitosAtuais,
+                      habitosSaudaveis: checked,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="habitos-saudaveis" className="font-normal">
                   Hábitos de Vida Saudáveis
@@ -68,6 +102,13 @@ function DocumentGenerator() {
                   id="habitos-tabagismo"
                   name="habitos-tabagismo"
                   className="cursor-pointer"
+                  checked={habitos.tabagismo}
+                  onCheckedChange={(checked) =>
+                    setHabitos((habitosAtuais) => ({
+                      ...habitosAtuais,
+                      tabagismo: checked,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="habitos-tabagismo" className="font-normal">
                   Tabagismo
@@ -78,6 +119,13 @@ function DocumentGenerator() {
                   id="habitos-sedentarismo"
                   name="habitos-sedentarismo"
                   className="cursor-pointer"
+                  checked={habitos.sedentarismo}
+                  onCheckedChange={(checked) =>
+                    setHabitos((habitosAtuais) => ({
+                      ...habitosAtuais,
+                      sedentarismo: checked,
+                    }))
+                  }
                 />
                 <FieldLabel
                   htmlFor="habitos-sedentarismo"
@@ -91,6 +139,13 @@ function DocumentGenerator() {
                   id="habitos-etilismo"
                   name="habitos-etilismo"
                   className="cursor-pointer"
+                  checked={habitos.etilismo}
+                  onCheckedChange={(checked) =>
+                    setHabitos((habitosAtuais) => ({
+                      ...habitosAtuais,
+                      etilismo: checked,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="habitos-etilismo" className="font-normal">
                   Etilismo
@@ -110,6 +165,14 @@ function DocumentGenerator() {
                   id="cronico-dm"
                   name="cronico-dm"
                   className="cursor-pointer"
+                  checked={historicoCronico.dm}
+                  onCheckedChange={(checked) =>
+                    setHistoricoCronico((historicoAtual) => ({
+                      ...historicoAtual,
+                      dm: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="cronico-dm" className="font-normal">
                   DM
@@ -120,6 +183,14 @@ function DocumentGenerator() {
                   id="cronico-has"
                   name="cronico-has"
                   className="cursor-pointer"
+                  checked={historicoCronico.has}
+                  onCheckedChange={(checked) =>
+                    setHistoricoCronico((historicoAtual) => ({
+                      ...historicoAtual,
+                      has: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="cronico-has" className="font-normal">
                   HAS
@@ -130,6 +201,14 @@ function DocumentGenerator() {
                   id="cronico-dislipidemia"
                   name="cronico-dislipidemia"
                   className="cursor-pointer"
+                  checked={historicoCronico.dislipidemia}
+                  onCheckedChange={(checked) =>
+                    setHistoricoCronico((historicoAtual) => ({
+                      ...historicoAtual,
+                      dislipidemia: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel
                   htmlFor="cronico-dislipidemia"
@@ -143,6 +222,22 @@ function DocumentGenerator() {
                   id="cronico-nenhuma"
                   name="cronico-nenhuma"
                   className="cursor-pointer"
+                  checked={historicoCronico.nenhuma}
+                  onCheckedChange={(checked) => {
+                    if (checked) {
+                      setHistoricoCronico({
+                        dm: false,
+                        has: false,
+                        dislipidemia: false,
+                        nenhuma: true,
+                      })
+                    } else {
+                      setHistoricoCronico((historicoAtual) => ({
+                        ...historicoAtual,
+                        nenhuma: false,
+                      }))
+                    }
+                  }}
                 />
                 <FieldLabel htmlFor="cronico-nenhuma" className="font-normal">
                   Nenhuma
@@ -162,6 +257,14 @@ function DocumentGenerator() {
                   id="familiar-dm"
                   name="familiar-dm"
                   className="cursor-pointer"
+                  checked={historicoFamiliar.dm}
+                  onCheckedChange={(checked) =>
+                    setHistoricoFamiliar((familiarAtual) => ({
+                      ...familiarAtual,
+                      dm: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="familiar-dm" className="font-normal">
                   DM
@@ -172,6 +275,14 @@ function DocumentGenerator() {
                   id="familiar-has"
                   name="familiar-has"
                   className="cursor-pointer"
+                  checked={historicoFamiliar.has}
+                  onCheckedChange={(checked) =>
+                    setHistoricoFamiliar((familiarAtual) => ({
+                      ...familiarAtual,
+                      has: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel htmlFor="familiar-has" className="font-normal">
                   HAS
@@ -182,6 +293,14 @@ function DocumentGenerator() {
                   id="familiar-dislipidemia"
                   name="familiar-dislipidemia"
                   className="cursor-pointer"
+                  checked={historicoFamiliar.dislipidemia}
+                  onCheckedChange={(checked) =>
+                    setHistoricoFamiliar((familiarAtual) => ({
+                      ...familiarAtual,
+                      dislipidemia: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel
                   htmlFor="familiar-dislipidemia"
@@ -195,6 +314,14 @@ function DocumentGenerator() {
                   id="familiar-coronariana"
                   name="familiar-coronariana"
                   className="cursor-pointer"
+                  checked={historicoFamiliar.coronariana}
+                  onCheckedChange={(checked) =>
+                    setHistoricoFamiliar((familiarAtual) => ({
+                      ...familiarAtual,
+                      coronariana: checked,
+                      nenhuma: false,
+                    }))
+                  }
                 />
                 <FieldLabel
                   htmlFor="familiar-coronariana"
@@ -208,6 +335,23 @@ function DocumentGenerator() {
                   id="familiar-nenhuma"
                   name="familiar-nenhuma"
                   className="cursor-pointer"
+                  checked={historicoFamiliar.nenhuma}
+                  onCheckedChange={(checked) => {
+                    if (checked) {
+                      setHistoricoFamiliar({
+                        dm: false,
+                        has: false,
+                        dislipidemia: false,
+                        coronariana: false,
+                        nenhuma: true,
+                      })
+                    } else {
+                      setHistoricoFamiliar((familiarAtual) => ({
+                        ...familiarAtual,
+                        nenhuma: false,
+                      }))
+                    }
+                  }}
                 />
                 <FieldLabel htmlFor="familiar-nenhuma" className="font-normal">
                   Nenhuma
